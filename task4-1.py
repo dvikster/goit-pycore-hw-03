72ec34e617fa6b143af5cfe2d79d6853c55da877
Viktoria <dvikster@gmail.com>
2024-11-02 10:37:22 +0200

    Update task4-1.py

--- task4-1.py
+++ task4-1.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    # Перетворення рядка дати у форматі 'YYYY-MM-DD' на об'єкт datetime
-    given_date = datetime.strptime(date, '%Y-%m-%d').date()
+    try:
+        # Перетворення рядка дати у форматі 'YYYY-MM-DD' на об'єкт datetime
+        given_date = datetime.strptime(date, '%Y-%m-%d').date()
+    except ValueError:
+        return "Невірний формат дати. Використовуйте формат 'YYYY-MM-DD'."
+
     # Отримання поточної дати
     today = datetime.today().date()
     # Обчислення різниці між поточною датою і заданою датою
@@ -11,10 +15,6 @@ def get_days_from_today(date):
     return delta.days
 
 # Приклади використання
-print(get_days_from_today('2024-10-20'))  # Виведе кількість днів від '2020-10-09' до сьогодні
+print(get_days_from_today('2024-10-20'))  # Виведе кількість днів від '2024-10-20' до сьогодні
 print(get_days_from_today('2024-11-10'))  # Виведе від'ємне значення для дати в майбутньому
-
-
-
-
-
+print(get_days_from_today('20-10-2024'))  # Виведе повідомлення про помилку
